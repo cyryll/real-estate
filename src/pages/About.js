ed0b4713a0038615aeb0a7a610e3d85013cd9bab
cyryll <cyrylpaull@gmail.com>
2021-02-09 21:14:35 +0300

    about/rental pages

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+const About = () => {
+    return (
+        <>
+        <Cards/>
+        </>
+    )
+}
+
+export default About
---
+++ src/pages/Rentals.js
@@ -0,0 +1,9 @@
+const Rentals = () => {
+    return (
+        <>
+        <Cards/>
+        </>
+    )
+}
+
+export default Rentals
