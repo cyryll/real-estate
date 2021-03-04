6625331a1059fef7248a27713ba96f354c59c222
cyryll <cyrylpaull@gmail.com>
2021-03-04 14:59:08 +0300

    about

--- src/data/InfoData.js
+++ src/data/InfoData.js
@@ -1,5 +1,6 @@
 import ImageOne from '../images/interior1.jpg'
 import ImageThree from '../images/interior2.jpg'
+import img1 from '../images/about1.svg'
 export const InfoData = {
     heading: "Explore beautiful homes",
     paragraghOne: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis feugiat",
@@ -17,6 +18,17 @@ export const InfoTwo = {
     paragraghTwo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis feugiat",
     buttonLabel: 'View Homes',
     image: ImageThree,
+    reverse: true,
+    path: '/homes',
+    delay: 300
+}
+
+export const InfoThree = {
+    heading: "We Help You Find  Your Dream House",
+    paragraghOne: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis feugiat",
+    paragraghTwo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper dignissim cras tincidunt lobortis feugiat",
+    buttonLabel: 'Find Your Next Home',
+    image: img1,
     reverse: true,
     path: '/homes',
     delay: 300
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,13 @@
 import PageHero from "../components/Hero/PageHero"
+import InfoSection from "../components/InfoSection/InfoSection"
+import { InfoThree } from "../data/InfoData"
 const About = () => {
     const title = "About us"
     const url = require('../images/ImageTwo.jpg').default
     return (
         <>
         <PageHero title = {title} url={url}/>
+        <InfoSection {...InfoThree}/>
         </>
     )
 }
